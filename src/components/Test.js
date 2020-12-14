2199b3d2c5e32f7c72a4df34c6cbc5edd578c57d
trevor <winge159@umn.edu>
2020-12-13 20:51:10 -0600

    made some more changes

--- src/components/Test.js
+++ src/components/Test.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Test = ({name, body, image}) => {
   return(
-    <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl margin-10">
-      <div className="ml-6 pt-1">
-        <img class="object-contain md:object-scale-down h-48 w-full" src={image}></img>
+    <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl margin-10 md:flex" >
+      <div className="ml-6 pt-1 md:flex-shrink-0">
+        <img class="object-contain h-48 w-full md:w-48" src={image}></img>
         <h1 className="text-2xl text-green-700 leading-tight">
           {name}
         </h1>
